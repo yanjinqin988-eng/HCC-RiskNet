81be5c4ddcf80aaa75e5321a18a60ee90d38f1fe
yanjinqin988-eng <yanjinqin988@gmail.com>
2026-04-15 22:40:38 +0800

    Body composition metrics

---
+++ Step 2. Automatic tissue segmentation.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Define input and output directories
+set "input_dir=imagesTr"
+set "output_dir=labelsTr_tissue"
+
+:: Create output directory if it doesn't exist
+if not exist "%output_dir%" mkdir "%output_dir%"
+
+:: Loop through all .nii.gz files in input directory
+for %%F in ("%input_dir%\*.nii.gz") do (
+    set "filename=%%~nxF"
+    set "input_path=%input_dir%\!filename!"
+    set "output_path=%output_dir%\!filename!"
+    echo Processing: !filename!
+    TotalSegmentator -i "!input_path!" -o "!output_path!" --ml -ta tissue_types_mr)
+
+echo Done.
+pause
